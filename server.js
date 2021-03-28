470d486aa33d6d17a338c10d338c51671d0af9ab
andrewy9 <andrew.yang993@gmail.com>
2021-03-29 00:38:23 +1300

    retrying with connectionstring

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-import express from 'express'//const express = require('express') in CommonJs
+import express from 'express' //const express = require('express') in CommonJs
 import bcrypt from 'bcrypt-nodejs'
 import cors from 'cors'
 import knex from 'knex'
-import _ from './env.js'
+// import _ from './env.js'
 import { handleRegister } from './controllers/register.js'
 import { handleSignin } from './controllers/signin.js'
 import { handleProfile } from './controllers/profile.js'
 import { handleImage, handleApiCall } from './controllers/image.js'
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: `${process.env.DB_USER}`,
-    password: `${process.env.DB_PSWD}`,
-    database: 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
